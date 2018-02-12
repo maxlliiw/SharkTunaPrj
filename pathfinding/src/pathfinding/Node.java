cf9a29933b18bfe53b04d338c24a903969d549c6
Will <Will@169.254.237.83>
2018-02-12 10:01:11 -0500

    Added comments.

--- pathfinding/src/pathfinding/Node.java
+++ pathfinding/src/pathfinding/Node.java
@@ -4,34 +4,51 @@ public class Node {
     int x;
     int y;
     String name;
-    
+    /*
+     * Instantiates variables used for the Node object.
+     */
     Node(int x, int y, String name) {
+    	/*
+    	 * Constructs Node class object with three parameters.
+    	 * int x - Stores a int x so it knows its x position on a 2D Array gameboard.
+    	 * int y - Stores a int y so it knows its y position on a 2D Array gameboard.
+    	 * String name - Stores the name of the object so we can return it from memory.
+    	 */
         this.x = x;
         this.y = y;
         this.name = name;
+        /*
+         * Assigns the variables used by a Node object based on the given parameters.
+         */
     }
     public int getX()
     {
     	return x;
+    	//Returns x for the Node object.
     }
     public int getY()
     {
     	return y;
+    	//Returns y for the Node object.
     }
     public String getName()
     {
     	return name;
+    	//Returns name for the Node object.
     }
     public void setName(String name)
     {
     	this.name=name;
+    	//Changes the name of the object.
     }
     public void changeX(int x)
     {
     	this.x=x;
+    	//Changes the x value for the object.
     }
     public void changeY(int y)
     {
     	this.y=y;
+    	//Changes the y value for the object.
     }
 }
--- pathfinding/src/pathfinding/Path.java
+++ pathfinding/src/pathfinding/Path.java
@@ -1,16 +1,21 @@
-package pathfinding;
+package pathfinding; //Organized group of classes named pathfinding
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.ArrayList; //Imports the ArrayList package from the java.util library
 
 public class Path
 {
+	
 	int gridsize = 4;
-	int move = 1;
+	//Initialize Integer for the gridsize, cana be changed to determine the Square size of the gameboard
 	ArrayList<Node> nodes = new ArrayList<Node>();
+	//Create an ArrayList named nodes that contains Node objects to fill gameboard.
 	public Path()
 	{
 		Node[][] grid = new Node[gridsize][gridsize];
+		/*
+		 * Creates a 2D Array named grid that stores Node objects.
+		 * Used to create our gameboard and determined upon the gridsize integer.
+		 */
 		
 		for(int i = 0; i<gridsize; i++)
 		{
@@ -23,15 +28,26 @@ public class Path
 				}
 			}
 		}
+		/*
+		 * Creates and Adds Node objects with name "Ocean" to the ArrayList nodes then...
+		 * retrives those objects stored in the ArrayList then stores them in our 2D array gameboard.
+		 */
 		
 		
-		Node tuna = new Node(3,3,"Tunaa");
+		Node tuna = new Node(3,3,"Tuna ");
 		Node shark = new Node(0,0,"Shark");
+		
 		grid[tuna.getX()][tuna.getY()] = tuna;
 		grid[shark.getX()][shark.getY()] = shark;
 		
-		
-		
+		/*
+		 * Creates Node objects for the shark and tuna that store an int x, int y, String name.
+		 * 
+		 * Shark and tuna contain x and y variables so the object stores its location.
+		 * 
+		 * The shark and tuna get placed in the gameboard with their location determined...
+		 * by the Node objects stored x and y value.
+		 */
 		
 		while(!((shark.getX()==tuna.getX())&&(shark.getY()==tuna.getY())))
 		{
@@ -58,9 +74,27 @@ public class Path
 			shark.changeY(shark.getY()-1);
 			grid[shark.getX()][shark.getY()+1]= new Node(shark.getX(),shark.getY()+1,"Ocean");
 		}
+		/*
+		 * Algorithm to allow the shark to move closer to the tuna determined by the...
+		 * stored x and y values in the Node objects of shark and tuna.
+		 * 
+		 * Updates the x and y values of the shark and tuna objects and replaces its previous...
+		 * position in the grid with a new Node object with its name being "Ocean"
+		 * 
+		 * 
+		 * 
+		 * Needs logic bounds for the Shark and Tuna so the Node objects int x, and int y aren't...
+		 * out of bounds for the 2D array grid once the Tuna starts to move.
+		 * 
+		 * Needs Algorithm so the Tuna can move.
+		 */
 		
 		grid[tuna.getX()][tuna.getY()] = tuna;
 		grid[shark.getX()][shark.getY()] = shark;
+		/*
+		 * When the move has been completed the 2D array grid gets updated
+		 * and stores the tuna and shark Node objects.
+		 */
 		
 		for(int m = 0;m<gridsize;m++)
 		{
@@ -70,7 +104,12 @@ public class Path
 			}
 			System.out.println();
 		}
+		/*
+		 * Prints out all the strings contained in the Node objects which are...
+		 * stored in the 2D array grid.
+		 */
 		System.out.println();
+		//Prints a line between turns for readablity.
 		
 		}
 		
@@ -79,8 +118,15 @@ public class Path
 		
 	
 	public static void main(String[] args)
+	 /*
+	   *Creates a method named main that is accessable in the package movietest
+	   *Method is class related
+	   *Method does not return any value
+	   *Contains a paramater of an String array type called args
+	   */
 	{
 		Path way = new Path();
+		//main method runs new object of type Path named way
 		
 	}
 }
